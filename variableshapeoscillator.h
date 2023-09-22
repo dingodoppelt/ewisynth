941b1060c82a743e67516bbfd9a4e0842dc74a8b
dingodoppelt <62596379+dingodoppelt@users.noreply.github.com>
2023-09-22 17:36:44 +0200

    add copyright

--- variableshapeoscillator.h
+++ variableshapeoscillator.h
@@ -1,3 +1,16 @@
+{
+/**         
+       @brief Variable Waveshape Oscillator. 
+       @author Ben Sergentanis
+       @date Dec 2020 
+       Continuously variable waveform. \n \n
+       Ported from pichenettes/eurorack/plaits/dsp/oscillator/variable_shape_oscillator.h \n 
+       \n to an independent module. \n
+       Original code written by Emilie Gillet in 2016. \n
+       
+       made into a single file and slimmed down by Nils Brederlow in 2023
+*/
+
 class VariableShapeOscillator
 {
 public:
